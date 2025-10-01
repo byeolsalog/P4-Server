6a311ffab318aad35796b89768653f9aaf595049
byeolsalog <syrup272@gmail.com>
2025-10-01 18:12:41 +0900

    t

--- start_server.sh
+++ start_server.sh
@@ -2,11 +2,11 @@
 set -e
 
 echo "[START] WebServer 실행"
-dotnet WebServer/WebServer.dll > webserver.log 2>&1 &
+dotnet WebServer/bin/Debug/net8.0/WebServer.dll > webserver.log 2>&1 &
 echo $! > webserver.pid
 
 echo "[START] GameServer 실행"
-dotnet GameServer/GameServer.dll > gameserver.log 2>&1 &
+dotnet GameServer/bin/Debug/net8.0/GameServer.dll > gameserver.log 2>&1 &
 echo $! > gameserver.pid
 
 echo "[DONE] 두 서버가 실행되었습니다."
